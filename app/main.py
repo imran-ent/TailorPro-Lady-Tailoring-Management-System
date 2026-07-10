876167fa89941262faa8033142a76960de62a971
Mohammed imran <imranambassadorofislam@gmail.com>
2026-07-10 19:44:31 +0530

    Update CORS allowed origins
    Added two new allowed origins for CORS.

--- app/main.py
+++ app/main.py
@@ -18,6 +18,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://tailorpro-lady-tailoring-management-ja63.onrender.com",
+        "https://tailor-pro-lady-tailoring-managemen.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
